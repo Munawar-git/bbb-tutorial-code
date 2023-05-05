f1be90c683cc1fe31f58f3f8b081562e8b5c8230
Munawar <munawerali1993@gmail.com>
2023-05-05 18:53:57 +0200

    example bbb code with header file

--- bbb-example.c
+++ bbb-example.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+ #include <stdio.h>
+#include "bbb-example.h"
 
-int main(void) {
-    printf("Hello, world!\n");
-    return 0;
+int main() {
+  #ifdef WELCOME
+    #if WELCOME == y
+      printf("Welcome to Yocto Tutorial.\n");
+    #endif
+  #endif
+
+  #ifdef HELLO
+    #if HELLO == y
+      printf("Hello there, I am an example code :)\n");
+    #endif
+  #endif
+
+  #ifdef BYE
+    #if BYE == y
+      printf("Bye, keep learning!!\n");
+    #endif
+  #endif
+
+  return 0;
 }
